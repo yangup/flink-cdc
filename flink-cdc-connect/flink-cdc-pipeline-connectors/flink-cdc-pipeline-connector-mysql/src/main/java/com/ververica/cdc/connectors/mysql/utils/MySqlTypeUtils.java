bc7f864b5a3392a35a3c41919f5e803ca3c36180
yangpu <yangpu@ksmglobal.tech>
2024-05-24 11:45:40 +0800

    a b

--- flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-mysql/src/main/java/com/ververica/cdc/connectors/mysql/utils/MySqlTypeUtils.java
+++ flink-cdc-connect/flink-cdc-pipeline-connectors/flink-cdc-pipeline-connector-mysql/src/main/java/com/ververica/cdc/connectors/mysql/utils/MySqlTypeUtils.java
@@ -133,7 +133,8 @@ public class MySqlTypeUtils {
                 // user should not use tinyint(1) to store number although jdbc url parameter
                 // tinyInt1isBit=false can help change the return value, it's not a general way
                 // btw: mybatis and mysql-connector-java map tinyint(1) to boolean by default
-//                return column.length() == 1 ? DataTypes.BOOLEAN() : DataTypes.TINYINT();
+                //                return column.length() == 1 ? DataTypes.BOOLEAN() :
+                // DataTypes.TINYINT();
                 // tinyint 直接转 tinyint 不用 boolean 的 数值
                 return DataTypes.TINYINT();
             case TINYINT_UNSIGNED:
@@ -199,7 +200,7 @@ public class MySqlTypeUtils {
                         : DataTypes.TIMESTAMP_LTZ(0);
             case CHAR:
                 // chart 使用 VARCHAR 来存储
-//                return DataTypes.CHAR(column.length());
+                //                return DataTypes.CHAR(column.length());
             case VARCHAR:
                 return DataTypes.VARCHAR(column.length());
             case TINYTEXT:
@@ -235,6 +236,5 @@ public class MySqlTypeUtils {
         }
     }
 
-    private MySqlTypeUtils() {
-    }
+    private MySqlTypeUtils() {}
 }
